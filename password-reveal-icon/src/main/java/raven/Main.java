d1e2faf3fb056732991f25c0d0afe276ba81076b
Raven Laing <58245926+DJ-Raven@users.noreply.github.com>
2024-12-12 00:28:46 +0700

    Add password reveal icon project

--- password-reveal-icon/src/main/java/raven/Main.java
+++ password-reveal-icon/src/main/java/raven/Main.java
@@ -1,5 +1,6 @@
 package raven;
 
+import com.formdev.flatlaf.FlatClientProperties;
 import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.FlatLaf;
 import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
@@ -15,20 +16,27 @@ public class Main extends JFrame {
         setSize(new Dimension(1000, 800));
         setLocationRelativeTo(null);
         setLayout(new MigLayout("al center center,wrap"));
-        add(new JPasswordField(), "w 250");
-        add(new JButton("Hello") {
+
+        JPasswordField txtPass = new JPasswordField();
+        txtPass.putClientProperty(FlatClientProperties.STYLE, "" +
+                "showRevealButton:true;");
+
+
+        add(txtPass, "width 250");
+
+        add(new JButton("Login") {
             @Override
             public boolean isDefaultButton() {
                 return true;
             }
-        }, "w 250");
+        }, "width 250");
     }
 
     public static void main(String[] args) {
         FlatRobotoFont.install();
         FlatLaf.registerCustomDefaultsSource("raven.themes");
         FlatDarculaLaf.setup();
-        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 20));
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
         EventQueue.invokeLater(() -> new Main().setVisible(true));
     }
 }
---
+++ password-reveal-icon/src/main/java/raven/icon/PasswordRevealIcon.java
@@ -0,0 +1,69 @@
+package raven.icon;
+
+import com.formdev.flatlaf.extras.FlatSVGIcon;
+import com.formdev.flatlaf.util.AnimatedIcon;
+import com.formdev.flatlaf.util.UIScale;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Line2D;
+
+public class PasswordRevealIcon implements AnimatedIcon {
+
+    private final Icon icon;
+    private final int space;
+
+    public PasswordRevealIcon() {
+        this(new FlatSVGIcon("raven/icon/eye.svg", 0.75f), 3);
+    }
+
+    public PasswordRevealIcon(Icon icon, int space) {
+        this.icon = icon;
+        this.space = space;
+    }
+
+    @Override
+    public void paintIconAnimated(Component c, Graphics g, int x, int y, float animatedValue) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        int s = UIScale.scale(space);
+        icon.paintIcon(c, g2, x, y);
+        if (animatedValue > 0) {
+            float startX = x + s;
+            float startY = y + getIconHeight() - s;
+
+            float endX = x + getIconWidth() - s;
+            float endY = y + s;
+
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+
+            Shape shape = new Line2D.Float(startX, startY, startX + (endX - startX) * animatedValue, startY + (endY - startY) * animatedValue);
+
+            drawLine(g2, shape, c.getParent().getBackground(), 4f);
+            drawLine(g2, shape, new Color(150, 150, 150), 1.5f);
+        }
+
+        g2.dispose();
+    }
+
+    private void drawLine(Graphics2D g2, Shape shape, Color color, float size) {
+        g2.setColor(color);
+        g2.setStroke(new BasicStroke(UIScale.scale(size), BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+        g2.draw(shape);
+    }
+
+    @Override
+    public float getValue(Component c) {
+        return ((AbstractButton) c).isSelected() ? 0 : 1;
+    }
+
+    @Override
+    public int getIconWidth() {
+        return icon.getIconWidth();
+    }
+
+    @Override
+    public int getIconHeight() {
+        return icon.getIconHeight();
+    }
+}
